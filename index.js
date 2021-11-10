310b7dfa272c7dac9a8cfcf90dc5877daa7c1293
mdmaruf641 <mdmarufmoni50@gmail.com>
2021-11-10 21:08:16 +0600

    product add to the database option added

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const { MongoClient } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gct3l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -17,7 +19,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    console.log("connected");
+    const database = client.db("carNation");
+    const productsCollection = database.collection("products");
+
+    // post api
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      console.log("hit the post", product);
+
+      const result = await productsCollection.insertOne(product);
+      console.log(result);
+      res.json(result);
+    });
   } finally {
     // await client.close()
   }
